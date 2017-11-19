98a56bc111eb7f05a075b2cae493973f6a247521
dav <dav2070@web.de>
2017-11-19 19:41:42 +0000

    Add Event logging

--- lib/dav/analytics.rb
+++ lib/dav/analytics.rb
@@ -1,17 +1,21 @@
 module Dav
-   class Analytics
-      
-      
-      def initialize(attributes)
-         
-      end
-   end
-   
    class Event
-      attr_accessor :name
+      attr_accessor :name, :id
       
       def initialize(attributes)
-         
+         @name = attributes["name"]
+         @id = attributes["id"]
       end
+      
+      def self.log(auth, app, name)
+         create_event_url = Dav::API_URL + 'analytics/event?name=' + name + '&app_id=' + app.id.to_s
+         json = send_http_request(create_event_url, create_auth_token(auth), "POST")
+         event = Event.new(json)
+      end
+      
+      #def self.get(auth, name)
+      #   get_event_url = Dav::API_URL + 'analytics/event?name=' + name
+      #   json = send_http_request(get_event_url, create_auth_token(auth), "GET")
+      #end
    end
 end
--- lib/dav/app.rb
+++ lib/dav/app.rb
@@ -1,11 +1,12 @@
 module Dav
-   attr_accessor :name, :description, :published
-   
    class App
+      attr_accessor :name, :description, :published, :id
+      
       def initialize(attributes)
          @name = attributes["name"]
          @description = attributes["description"]
          @published = attributes["description"]
+         @id = attributes["id"]
       end
       
       def self.create(user, name, desc)
@@ -18,6 +19,12 @@ module Dav
          get_app_url = Dav::API_URL + 'apps/app/' + id.to_s
          json = send_http_request(get_app_url, user.jwt, "GET")
          app = App.new(json)
+      end
+      
+      def log_event(auth, name)
+         create_event_url = Dav::API_URL + 'analytics/event?name=' + name + '&app_id=' + @id.to_s
+         json = send_http_request(create_event_url, create_auth_token(auth), "POST")
+         event = Event.new(json)
       end
    end
 end
