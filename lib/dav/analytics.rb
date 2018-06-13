b2a8b41f123463a87a5846f4f96890ab9b7d87be
Dav <dav2070@web.de>
2018-06-13 21:50:57 +0200

    Move Event class into new event.rb and add Analytics class with get_app and get_users

--- lib/dav/analytics.rb
+++ lib/dav/analytics.rb
@@ -1,70 +1,25 @@
 module Dav
-   class Event
-      attr_reader :id, :name, :logs
-      
-      def initialize(attributes)
-         @id = attributes["id"]
-         @name = attributes["name"]
-			@logs = attributes["logs"]
-      end
-      
-      def self.log(auth, app_id, name, data)
-			url = $api_url + "analytics/event?name=#{name}&app_id=#{app_id}"
-			if data
-				url += "&data=#{data}"
-			end
+	class Analytics
+		def self.get_app(jwt, id)
+			url = $api_url + "analytics/app/#{id}"
+			result = send_http_request(url, "GET", {"Authorization" => jwt}, nil)
 
-         result = send_http_request(url, "POST", {"Authorization" => create_auth_token(auth)}, nil)
-         if result["code"] == 201
-            event = Event.new(JSON.parse(result["body"]))
+			if result["code"] == 200
+            return JSON.parse(result["body"])
          else
             raise_error(JSON.parse(result["body"]))
          end
-      end
+		end
 
-      def self.get(jwt, id, app_id)
-         url = $api_url + "analytics/event/#{id}?app_id=#{app_id}"
-         result = send_http_request(url, "GET", {"Authorization" => jwt}, nil)
+		def self.get_users(jwt)
+			url = $api_url + "analytics/users"
+			result = send_http_request(url, "GET", {"Authorization" => jwt}, nil)
 
-         if result["code"] == 200
-            event = Event.new(JSON.parse(result["body"]))
+			if result["code"] == 200
+            return JSON.parse(result["body"])
          else
             raise_error(JSON.parse(result["body"]))
          end
-      end
-
-      def self.get_by_name(jwt, name, app_id)
-         url = $api_url + "analytics/event?name=#{name}&app_id=#{app_id}"
-         result = send_http_request(url, "GET", {"Authorization" => jwt}, nil)
-
-         if result["code"] == 200
-            event = Event.new(JSON.parse(result["body"]))
-         else
-            raise_error(JSON.parse(result["body"]))
-         end
-      end
-
-      def update(jwt, name)
-         url = $api_url + "analytics/event/#{@id}"
-         result = send_http_request(url, "PUT", {"Authorization" => jwt, "Content-Type" => "application/json"}, {name: name})
-
-         if result["code"] == 200
-            @name = name
-            return self
-         else
-            raise_error(JSON.parse(result["body"]))
-         end
-      end
-
-      def delete(jwt)
-         url = $api_url + "analytics/event/#{@id}"
-         result = send_http_request(url, "DELETE", {"Authorization" => jwt}, nil)
-
-         if result["code"] == 200
-            return true
-         else
-            raise_error(JSON.parse(result["body"]))
-         end
-      end
-   end
+		end
+	end
 end
