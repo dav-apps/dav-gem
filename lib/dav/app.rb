9567170d29b7d90be203dda3059b7b244095947c
Dav <dav2070@web.de>
2019-03-21 15:57:35 +0100

    Add App.get_active_users and return an array of active user objects

--- lib/dav/app.rb
+++ lib/dav/app.rb
@@ -35,6 +35,16 @@ module Dav
          end
       end
 
+      def get_active_users(jwt, start_timestamp = Time.now.to_i - 2629746, end_timestamp = Time.now.to_i)
+         url = $api_url + "apps/app/#{@id}/active_users?start=#{start_timestamp}&end=#{end_timestamp}"
+         result = send_http_request(url, "GET", {"Authorization" => jwt}, nil)
+         if result["code"] == 200
+            return convert_json_to_active_users_array(JSON.parse(result["body"])["days"])
+         else
+            raise_error(JSON.parse result["body"])
+         end
+      end
+
       def self.get_all_apps(auth)
          url = $api_url + "apps/apps/all"
          result = send_http_request(url, "GET", {"Authorization" => create_auth_token(auth)}, nil)
