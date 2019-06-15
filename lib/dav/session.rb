260dd74d5a96c85a09e7b6b36b9f51b49c3f39f8
David <dav2070@web.de>
2019-06-15 20:56:29 +0200

    Add device_name, device_type and device_os to session

--- lib/dav/session.rb
+++ lib/dav/session.rb
@@ -1,22 +1,25 @@
 module Dav
 	class Session
-		attr_accessor :id, :user_id, :app_id, :exp, :jwt
+		attr_accessor :id, :user_id, :app_id, :exp, :device_name, :device_type, :device_os, :jwt
 
 		def initialize(attributes)
 			@id = attributes["id"]
 			@user_id = attributes["user_id"]
 			@app_id = attributes["app_id"]
-         @exp = attributes["exp"]
+			@exp = attributes["exp"]
+			@device_name = attributes["device_name"]
+			@device_type = attributes["device_type"]
+			@device_os = attributes["device_os"]
          @jwt = attributes["jwt"]
 		end
 
-		def self.create(auth_token, email, password, api_key, app_id)
+		def self.create(auth_token, email, password, api_key, app_id, device_name, device_type, device_os)
 			url = $api_url + "auth/session"
 			result = send_http_request(
 				url, 
 				"POST", 
 				{"Authorization" => auth_token, "Content-Type" => "application/json"},
-				{"email" => email, "password" => password, "api_key" => api_key, "app_id" => app_id}
+				{"email" => email, "password" => password, "api_key" => api_key, "app_id" => app_id, "device_name" => device_name, "device_type" => device_type, "device_os" => device_os}
 			)
 			
 			if result["code"] == 201
